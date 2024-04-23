b7b93eebd5c3efa115af8fc5c4ef8bd515571671
nicolalaflamel <yukmaruyamu@gmail.com>
2024-04-23 21:10:54 +0900

    login and logout

--- flaskbook/apps/auth/forms.py
+++ flaskbook/apps/auth/forms.py
@@ -22,3 +22,17 @@ class SignUpForm(FlaskForm):
         "パスワード", validators=[DataRequired("パスワードは必須です。")]
     )
     submit = SubmitField("新規登録")
+
+
+class LoginForm(FlaskForm):
+    email = StringField(
+        "メールアドレス",
+        validators=[
+            DataRequired("メールアドレスは必須です。"),
+            Email("メールアドレスの形式で入力してください。"),
+        ],
+    )
+    password = PasswordField(
+        "パスワード", validators=[DataRequired("パスワードは必須です。")]
+    )
+    submit = SubmitField("ログイン")
